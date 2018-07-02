d8efb31fa32c47f678cc4399a22ac1ccad5ef1e6
ars791 <alirezaseif28@gmail.com>
2018-07-02 21:46:06 +0430

    1

--- quickstart/src/app/app.component.ts
+++ quickstart/src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: `template/app.component.html`,
 })
-export class AppComponent { name = 'Alireza'; }
+export class AppComponent { }
